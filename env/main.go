1842ecb9819f43f63a98b39339820af78841fa26
Nathan Bosscher <nate.bosscher@gmail.com>
2020-09-02 08:00:36 -0400

    add int/bool parsing to env

--- env/main.go
+++ env/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strconv"
 )
 
 var IsTesting bool
@@ -26,6 +27,24 @@ func Require(key string) string {
 	return value
 }
 
+func RequireInt(key string) int {
+	i, err := strconv.Atoi(Require(key))
+	if err != nil {
+		log.Fatal("invalid environment variable value '" + Require(key) + "' for key " + key)
+	}
+
+	return i
+}
+
+func RequireBool(key string) bool {
+	i, err := strconv.ParseBool(Require(key))
+	if err != nil {
+		log.Fatal("invalid environment variable value '" + Require(key) + "' for key " + key)
+	}
+
+	return i
+}
+
 func Optional(key string, defaultValue string) string {
 	v := os.Getenv(key)
 	if v != "" {
@@ -33,4 +52,32 @@ func Optional(key string, defaultValue string) string {
 	}
 
 	return defaultValue
+}
+
+func OptionalBool(key string, defaultValue bool) bool {
+	v := os.Getenv(key)
+	if v == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.ParseBool(v)
+	if err != nil {
+		log.Fatal("invalid environment variable value '" + v + "' for key " + key + " should be true,false or undefined")
+	}
+
+	return i
+}
+
+func OptionalInt(key string, defaultValue int) int {
+	v := os.Getenv(key)
+	if v == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		log.Fatal("invalid environment variable value '" + v + "' for key " + key + " should be a number or undefined")
+	}
+
+	return i
 }
