807773a9c3a2e1c446d3d158014ef01118a301a8
Nathan Bosscher <nate.bosscher@gmail.com>
2020-11-06 08:54:11 -0500

    Add some documentation for model methods

--- model/sqbuilder.go
+++ model/sqbuilder.go
@@ -21,6 +21,7 @@ func verboseLog(err error, query string, args ...interface{}) {
 	log.Println("query input: ", query, args)
 }
 
+// ExecContext runs the query without expecting any output
 func ExecContext(ctx context.Context, query string, args ...interface{}) error {
 	_, err := Tx(ctx).ExecContext(ctx, query, args...)
 	verboseLog(err, query, args...)
@@ -54,12 +55,15 @@ func Insert(ctx context.Context, query string, args ...interface{}) (id int64, e
 	return
 }
 
+// GetContext runs the query expecting exactly 1 resulting row. That row is scanned into dest.
 func GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
 	err := Tx(ctx).GetContext(ctx, dest, query, args...)
 	verboseLog(err, query, args...)
 	return err
 }
 
+// SelectContextString runs the query and scans the resulting rows into dest.
+// Dest must be a pointer to a array-type (e.g. *[]*Person)
 func SelectContextString(ctx context.Context, dest interface{}, sql string, args ...interface{}) error {
 	err := Tx(ctx).SelectContext(ctx, dest, sql, args...)
 	if err != nil {
@@ -71,22 +75,30 @@ func SelectContextString(ctx context.Context, dest interface{}, sql string, args
 	return nil
 }
 
+// SelectContext runs a query created with model.Builder.Select and scans the resulting
+// rows into dest.
+// Dest must be a pointer to a array-type (e.g. *[]*Person)
 func SelectContext(ctx context.Context, dest interface{}, qr sq.Sqlizer) error {
-	sql, args, err := qr.ToSql()
+	sqlStr, args, err := qr.ToSql()
 	if err != nil {
-		verboseLog(err, sql, args...)
+		verboseLog(err, sqlStr, args...)
 		return err
 	}
 
-	verboseLog(err, sql, args...)
-	return SelectContextString(ctx, dest, sql, args...)
+	verboseLog(err, sqlStr, args...)
+	return SelectContextString(ctx, dest, sqlStr, args...)
 }
 
+// QueryRowContext runs the query and expects exactly 1 row. The results can be collected
+// by calling .Scan() on the result
 func QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
 	verboseLog(nil, query, args...)
 	return Tx(ctx).QueryRowContext(ctx, query, args...)
 }
 
+// Builder is a pre-configured squirrel sql-builder instance.
+// Build queries (e.g. Builder.Select("col").From("table")...) and pass the builder to SelectContext() to get the
+// results.
 var Builder = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
 
 // UnionBuilder is a (rather hack) implementation of Unions for squirrel query builder. They
