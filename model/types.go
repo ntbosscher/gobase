5d00f64f94afac369a88660f61a5443a86a79e07
Nathan Bosscher <nate.bosscher@gmail.com>
2020-11-17 11:10:45 -0500

    add model.Date to deal with date types

--- model/types.go
+++ model/types.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"encoding/json"
+	"time"
+)
+
 type NullByteArray struct {
 	Valid bool
 	Bytes []byte
@@ -14,4 +19,28 @@ func (n *NullByteArray) Scan(value interface{}) error {
 	n.Valid = true
 	n.Bytes = value.([]byte)
 	return nil
+}
+
+type Date time.Time
+
+const dateFormat = "2006-01-02"
+
+func (d *Date) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	tm, err := time.Parse(dateFormat, s)
+	if err != nil {
+		return err
+	}
+
+	*d = Date(tm)
+	return nil
+}
+
+func (d Date) MarshalJSON() ([]byte, error) {
+	s := time.Time(d).Format(dateFormat)
+	return json.Marshal(s)
 }
