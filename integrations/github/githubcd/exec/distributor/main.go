a29602208510c7ce5ee8803930d538e445376d93
Nathan Bosscher <nate.bosscher@gmail.com>
2021-04-21 15:29:16 -0400

    update githubcd to no longer use nginx configuration switching, just forward upgrade requests

--- integrations/github/githubcd/exec/distributor/main.go
+++ integrations/github/githubcd/exec/distributor/main.go
@@ -6,24 +6,23 @@ import (
 	"github.com/ntbosscher/gobase/env"
 	"github.com/ntbosscher/gobase/er"
 	"github.com/ntbosscher/gobase/httpdefaults"
-	"github.com/ntbosscher/gobase/integrations/github/githubcd/exec/distributor/nginx"
 	"github.com/ntbosscher/gobase/integrations/github/githubcd/githubcdutil"
 	"github.com/ntbosscher/gobase/res"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	"strings"
 	"time"
 )
 
-var configFile string
 var secret string
 var remoteHookPath string
+var servers []string
 
 func init() {
-	configFile = env.Require("NGINX_BACKEND_CONFIG")
 	secret = env.Require("GITHUB_SECRET")
 	remoteHookPath = env.Require("REMOTE_HOOK_PATH")
+	servers = strings.Split(env.Require("SERVERS"), ",")
 }
 
 func main() {
@@ -48,24 +47,12 @@ func handler(rq *res.Request) res.Responder {
 		return res.WithCodeAndMessage(403, "bad signature")
 	}
 
-	data, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		log.Println("failed to read nginx config:", err)
-		return res.InternalServerError("failed to update")
-	}
-
-	file, servers, err := nginx.ParseBackends(data)
-	if err != nil {
-		log.Println("failed to read nginx config:", err)
-		return res.InternalServerError("failed to update")
-	}
-
 	go func() {
 		defer er.HandleErrors(func(input *er.HandlerInput) {
 			log.Println(input.Error, input.StackTrace)
 		})
 
-		err := performUpgrade(body, file, servers)
+		err := performUpgrade(body, servers)
 		if err != nil {
 			log.Println(err)
 		}
@@ -77,54 +64,11 @@ func handler(rq *res.Request) res.Responder {
 	})
 }
 
-func performUpgrade(requestBody []byte, file nginx.File, servers []*nginx.ServerRow) error {
+func performUpgrade(requestBody []byte, servers []string) error {
 
 	log.Println("starting upgrade")
 
-	serverMap := map[string][]*nginx.ServerRow{}
-	for _, server := range servers {
-		// ignore servers that are already down
-		if server.Status == "down" {
-			continue
-		}
-
-		serverMap[server.Name] = append(serverMap[server.Name], server)
-	}
-
-	normalExit := false
-
-	// restore config if something fails
-	defer func() {
-		if normalExit {
-			return
-		}
-
-		log.Println("restoring after failure")
-		for _, values := range serverMap {
-			for _, server := range values {
-				server.Status = ""
-			}
-		}
-
-		if err := updateNginxConfig(file); err != nil {
-			log.Println("failed to restore nginx config:", err)
-		}
-	}()
-
-	for name, values := range serverMap {
-
-		log.Println(name, "taking down backend...")
-
-		for _, value := range values {
-			value.Status = "down"
-		}
-
-		if err := updateNginxConfig(file); err != nil {
-			log.Println("failed to update config:", err)
-		}
-
-		log.Println(name, "waiting for requests to drain")
-		<-time.After(10 * time.Second)
+	for _, name := range servers {
 
 		log.Println("sending", name, "the update webhook...")
 
@@ -151,6 +95,7 @@ func performUpgrade(requestBody []byte, file nginx.File, servers []*nginx.Server
 		<-time.After(5 * time.Second)
 
 		timeout := time.Now().Add(30 * time.Second)
+
 		for time.Now().Before(timeout) {
 			resp, err := http.Get("http://" + name + "/")
 			if err != nil {
@@ -163,29 +108,9 @@ func performUpgrade(requestBody []byte, file nginx.File, servers []*nginx.Server
 			break
 		}
 
-		log.Println(name, "restoring nginx config...")
-		for _, value := range values {
-			value.Status = ""
-		}
-
-		if err := updateNginxConfig(file); err != nil {
-			log.Println("failed to update config:", err)
-		}
-
 		<-time.After(1 * time.Second)
 	}
 
 	log.Println("done deployment")
-
-	normalExit = true
 	return nil
-}
-
-func updateNginxConfig(file nginx.File) error {
-	err := ioutil.WriteFile(configFile, []byte(file.String()), os.ModePerm)
-	if err != nil {
-		return err
-	}
-
-	return nginx.ReloadConfig()
 }
