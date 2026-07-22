a39b16586f193eb34f8fe2c20a215b0d00f697de
Nathan Bosscher <nate.bosscher@gmail.com>
2026-07-22 16:47:33 -0400

    ratelimit: bugfix on IsLimited

--- ratelimit/main.go
+++ ratelimit/main.go
@@ -3,6 +3,7 @@ package ratelimit
 import (
 	"context"
 	"errors"
+	"sync"
 	"time"
 )
 
@@ -11,6 +12,7 @@ func New(count int, interval time.Duration) *Limiter {
 		count:  count,
 		bucket: make(chan bool, count),
 		tc:     time.NewTicker(interval),
+		done:   make(chan struct{}),
 	}
 
 	go l.start()
@@ -19,13 +21,19 @@ func New(count int, interval time.Duration) *Limiter {
 }
 
 type Limiter struct {
-	count  int
-	bucket chan bool
-	tc     *time.Ticker
+	count    int
+	bucket   chan bool
+	tc       *time.Ticker
+	done     chan struct{}
+	stopOnce sync.Once
 }
 
+// Stop halts the refill ticker and the start() goroutine. Safe to call multiple times.
 func (l *Limiter) Stop() {
-	l.tc.Stop()
+	l.stopOnce.Do(func() {
+		l.tc.Stop()
+		close(l.done)
+	})
 }
 
 func (l *Limiter) start() {
@@ -37,7 +45,11 @@ func (l *Limiter) start() {
 			}
 		}
 
-		<-l.tc.C
+		select {
+		case <-l.tc.C:
+		case <-l.done:
+			return
+		}
 	}
 }
 
@@ -59,10 +71,16 @@ func (l *Limiter) TakeContext(ctx context.Context) error {
 	}
 }
 
+// IsLimited reports whether the caller would be rate-limited, without consuming a token.
+// The put-back is non-blocking: between the take and the put-back the start() goroutine may
+// have refilled the bucket to capacity, so a blocking send here could hang forever.
 func (l *Limiter) IsLimited() error {
 	select {
 	case <-l.bucket:
-		l.bucket <- true
+		select {
+		case l.bucket <- true:
+		default:
+		}
 		return nil
 	default:
 		return ErrRateLimited
